230df62c53833683ed8f0463d0115a17255e3323
Zachary Pahl <zp3tech@gmail.com>
2024-04-16 00:32:18 -0700

    BACKEND setup... django/bash issues
    at 1:04:17 in YouTube tutorial. changing AI_BLOG_APP ai_blog_app/settings.py

--- BACKEND/AI_BLOG_APP/tutorial-env/Scripts/activate.bat
+++ BACKEND/AI_BLOG_APP/tutorial-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pahlz\coding-projects\AI-Blog-Generator\BACKEND\ai_blog_project
+set VIRTUAL_ENV=C:\Users\pahlz\coding-projects\AI-Blog-Generator\BACKEND\ai_blog_app\tutorial-env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(ai_blog_project) %PROMPT%
+set PROMPT=(tutorial-env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(ai_blog_project) 
+set VIRTUAL_ENV_PROMPT=(tutorial-env) 
 
 :END
 if defined _OLD_CODEPAGE (
